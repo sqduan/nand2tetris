bd46a7c77d2ec4a6d4a88e2c3644e3af66db6648
sqduan <sqduan@mail.ustc.edu.cn>
2022-09-26 22:26:13 +0800

    Nand2tetris: Project4 keyboard fill program, finished basic code structure

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,86 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Pseudo-code
+// Init part
+// press  = 0
+// filled = 0
+//
+// if (M[KBD] != 0) {
+//     goto FILL_CHECK   
+// } else {
+//     goto RESET_CHECK
+// }
+//
+// FILL_CHECK
+// if (filled != 0) {
+//     goto KEY_CHECK
+// }
+
+(START)
+    @pressed
+    M = 0
+    @filled
+    M = 0
+    
+    @total_row
+    M = 512
+    @total_col
+    M = 32
+
+(KEYBOARD_CHECK)
+    @KBD
+    D = M            // D = M[KBD]
+    @FILL_CHECK
+    D; JNE           // Key pressed, goto FILL_CHECK
+
+    @RESET_CHECK     // Key not pressed, goto RESET_CHECK
+    0; JMP
+
+(FILL_CHECK)
+    @filled
+    D = M
+
+    @KEYBOARD_CHECK
+    D ; JNE          // Already filled, no need to fill
+
+    @FILL            // Not filled, go and fill the screen
+    0 ; JMP
+
+(RESET_CHECK)
+    @filled
+    D = M
+
+    @RESET           // Filled, Needs reset
+    D ; JNE
+
+    @KEYBOARD_CHECK  // Already reset, no need to reset
+    0 ; JMP
+
+(FILL)
+    @SCREEN
+    M = -1
+    @row
+    M = 0
+    @col
+    M = 0
+
+(FILL_FINISHED)
+    @filled
+    M = 1
+    @KEYBOARD_CHECK
+    0 ; JMP
+
+(RESET)
+    @SCREEN
+    M = 0
+    @row
+    M = 0
+    @col
+    M = 0
+
+(RESET_FINISHED)
+    @filled
+    M = 0
+    @KEYBOARD_CHECK
+    0 ; JMP
